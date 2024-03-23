d263800752b577b429a7ccca7b0bea1923d372f4
FelipeMaciel05 <felimorita05@gmail.com>
2024-03-23 01:40:08 -0300

    Proyectos terminador

---
+++ portfolio/src/Componentes/Bit.jsx
@@ -0,0 +1,27 @@
+function Bit(){
+    return (
+        <div className="modi">
+                            <div className="modi-box">
+                                <div className="modi-left">
+                                    <label className="mini-proyect">DEVELOPER</label>
+                                    <label className="big-proyect">Bit Assistant</label>
+                                    <label className="modi-description">Bit Assistant es un asistente virtual cuyo objetivo es responder a preguntas básicas y ayudar en la vida cotidiana.</label>
+                                    <div className="modi-links">
+                                        <div className="vercel">
+                                            <svg width="30%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M11.7057 13.7071L17.9986 7.41421V11H19.9986V4H12.9986V6H16.5844L10.2915 12.2929L11.7057 13.7071Z" fill="#fff"/>
+                                            <path d="M19.0015 18V14H17.0015V18H6.00146V7H10.0015V5H6.00146C4.8969 5 4.00146 5.89543 4.00146 7V18C4.00146 19.1046 4.8969 20 6.00146 20H17.0015C18.106 20 19.0015 19.1046 19.0015 18Z" fill="#fff"/>
+                                            </svg>
+                                            <a target="_blank" rel="noreferrer" href="https://drive.google.com/drive/folders/1ieVSAnVsAkeWoORp50nNfo9RtJuiLGkV?usp=sharing"><button>Folder</button></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="modi-right">
+                                    <div className="modi-png"></div>
+                                </div>
+                            </div>
+                        </div>
+    );
+}
+
+export default Bit;
--- portfolio/src/Componentes/Home.jsx
+++ portfolio/src/Componentes/Home.jsx
@@ -1,65 +1,120 @@
-import React from 'react';
-import './styles/home.css'
+import React, { useState } from 'react';
+import Modi from "./Modi";
+import Ort from "./Ort";
+import Bit from "./Bit";
+
+import './styles/home.css';
 
 function Home() {
+
+        const [selectedProject, setSelectedProject] = useState('all');
+    
+        const handleProjectChange = (project) => {
+            setSelectedProject(project);
+        };
+
     return (
         <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-            <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
-            <title>portfolio</title>
-        </head>
-        <body>
-            
-        <header>
-            <div className="box-header">
-                <div className="logo"> 
-                    <a href="#a" className="coloured">FELIPE</a>
-                </div>
-                <div className="items">
-                    <div className="item">
-                        <a href="#a">Home</a>
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
+            </head>
+            <body>
+                <header className="sticky">
+                    <div className="box-header">
+                        <div className="items">
+                            <div className="item">
+                                <a href="#home">Home</a>
+                            </div>
+                            <div className="item">
+                                <a href="#proyect">Proyects</a>
+                            </div>
+                            <div className="item">
+                                <a href="#a">Skills</a>
+                            </div>
+                            <div className="item">
+                                <a href="#b">Contact Me</a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="item">
-                        <a href="#a">About me</a>
-                    </div>
-                    <div className="item">
-                        <a href="#a">Proyects</a>
-                    </div>
-                    <div className="item">
-                        <a href="#b">Contact Me</a>
-                    </div>
-                </div>
-            </div>
-        </header>
+                </header>
 
-        <div className="box">
-            <div className="left">
-                <img src="/fotohombre.png" alt="foto" width="60%"/>                
-            </div>
-            <div className="right">
-                <div className="titles">
-                        <h1>I'm Felipe Maciel</h1>
-                        <h3>Front End Developer</h3>    
-                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
-                </div>
-                <div className="buttons">
-                    <div className="button-one">
-                        <a href="#a">Download CV</a>
+                <div className="box" id="home">
+                    <div className="left">
+                        <img src="/fotohombre.png" alt="foto" width="60%"/>                
                     </div>
-                    <div className="button-two">
-                        <a href="#b">Contact Me</a>
+                    <div className="right">
+                        <div className="titles">
+                            <p className="name">I'm Felipe Maciel</p>
+                            <p className="rol">Web Developer</p>    
+                            <p className="desc">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
+                        </div>
+                        <div className="buttons">
+                            <div className="button-one">
+                                <a href="#a"><button>Download CV</button></a>
+                            </div>
+                            <div className="button-two">
+                                <a href="#b"><button>Contact Me</button></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
 
-        </body>
+                <div className="proyecto" >
+                    <div className="box-proyect">
+                        <div className="mini-header" id="proyect">
+                            <label className="mini-proyect">PROYECTS</label>
+                            <label className="big-proyect">My featured projects</label>
+                            <div className="mini-items">
+                                <div className="mini-item">
+                                    <a href="#a" onClick={() => handleProjectChange('all')}>Todos</a>
+                                </div>
+                                <div className="mini-item">
+                                    <a href="#a" onClick={() => handleProjectChange('frontend')}>Front-End</a>
+                                </div>
+                                <div className="mini-item">
+                                    <a href="#a" onClick={() => handleProjectChange('uxui')}>UX-UI</a>
+                                </div>
+                                <div className="mini-item">
+                                    <a href="#a" onClick={() => handleProjectChange('developer')}>Developer</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        {selectedProject === 'all' && (
+                            <div>
+                                <Modi />
+                                <Ort />
+                                <Bit />
+                            </div>
+                        )}
+
+                        {selectedProject === 'frontend' && (
+                            <div>
+                                <Modi />
+                                <Ort />
+                            </div>
+                        )}
+
+                        {selectedProject === 'uxui' && (
+                            <div>
+                                <Modi />
+                            </div>
+                        )}
+
+                        {selectedProject === 'developer' && (
+                            <div>
+                                <Bit />
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </body>
         </html>
     );
 }
